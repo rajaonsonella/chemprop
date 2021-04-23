e325b8e483c660de5719b34b3107e8bc66884c58
rileyhickman <riley.hickman13@gmail.com>
2021-04-22 20:27:39 -0400

    testing notebook version of model

---
+++ rileys_tests/test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import deepchem as dc
+import chemprop
+
+#===============================================================================
+
+
+#===============================================================================
